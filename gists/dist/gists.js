66d0599c427fc5eb9de2831cf2ca0869b1b26540
Roy Segall <roy.segall@gizra.com>
2016-12-15 00:05:57 +0200

    Adding gists

--- gists/dist/gists.js
+++ gists/dist/gists.js
@@ -1,20 +1,70 @@
 var horizon = Horizon();
 horizon.connect();
 
-// Handeling the gists list.
-var gistsLists = new Vue({
-  el: '#gistsLists',
+const gists = horizon('gists');
+
+const Main = { template:
+  '<div class="ui form">'
+    + '<div class="field">'
+      + '<label>Gist title</label>'
+      + '<input type="text">'
+    + '</div>'
+    + '<div class="field">'
+      + '<label>Short Text</label>'
+      + '<textarea rows="30"></textarea>'
+    + '</div>'
+
+    + '<div class="ui submit button">Submit</div>'
+
+  +' </div>'
+};
+
+const Gist = {
+  template: '<div>A gist! {{ foo }}</div>',
+  created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.fetchData()
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      this.foo = 'foo';
+      gists.find(this.$route.params.id).fetch().subscribe(
+        (msg) => {
+          this.foo = 'a';
+        }
+      );
+    }
+  }
+};
+
+const routes = [
+  { path: '/', component: Main },
+  { path: '/gist/:id', component: Gist}
+];
+
+const router = new VueRouter({
+  routes
+});
+
+const mainApp = new Vue({
+  router,
   data: {
     gists: []
   }
-});
+}).$mount('#mainApp');
 
-const gists = horizon('gists');
 
 gists.watch({rawChanges: true}).subscribe(gists => {
 
   if (gists.new_val == undefined) {
     return;
   }
-  gistsLists.gists.push(gists.new_val)
+  gists.new_val.link = '/gist/' + gists.new_val.id;
+  mainApp.gists.push(gists.new_val)
 });
+
